eb481bac1f62a280960fdebacca666fe62ca95f6
JeanPierreTB <128437443+JeanPierreTB@users.noreply.github.com>
2023-11-09 10:25:59 -0500

    Cambio en Url

--- app/src/main/java/pe/edu/ulima/pm20232/aulavirtual/configs/constants.kt
+++ app/src/main/java/pe/edu/ulima/pm20232/aulavirtual/configs/constants.kt
@@ -1,4 +1,4 @@
 package pe.edu.ulima.pm20232.aulavirtual.configs
 
 // val BASE_URL: String = "http://192.168.1.27:3000/"
-val BASE_URL: String = "https://programamovil2023-trabajo3--jeanpierretb.repl.co/"
+val BASE_URL: String = "https://movilgrupo3-backend.adriulima.repl.co"
